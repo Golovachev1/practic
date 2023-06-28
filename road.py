64add901d9e40918e61a0988e162aff4574ed107
Golovachev1 <138000454+Golovachev1@users.noreply.github.com>
2023-06-28 17:15:47 +0700

    Add files via upload

---
+++ road.py
@@ -0,0 +1,8 @@
+class Road:
+    def __init__(self, number, start, end, medium, time_prib, time_otpr):
+        self.number = number
+        self.start = start
+        self.end = end
+        self.medium = medium
+        self.time_prib = time_prib
+        self.time_otpr = time_otpr
